f41c9495710382caa77cbfb5a5b1b1939e81d37e
Barbara Pušnar <barbara.pusnar@scng.si>
2024-07-19 09:23:05 +0200

    dokumentacija

--- OUporabnikih/obj/Debug/net8.0/OUporabnikih.AssemblyInfo.cs
+++ OUporabnikih/obj/Debug/net8.0/OUporabnikih.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OUporabnikih")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b12498a01d630fe3cf448ad5ed2d2e8b1ef288b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("OUporabnikih")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OUporabnikih")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TestMaui/obj/Debug/net8.0-ios/iossimulator-x64/TestMaui.AssemblyInfo.cs
+++ TestMaui/obj/Debug/net8.0-ios/iossimulator-x64/TestMaui.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestMaui")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b12498a01d630fe3cf448ad5ed2d2e8b1ef288b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestMaui")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestMaui")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
